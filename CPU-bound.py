200e115af4d0a0da6c37ab23bb7f720b14544154
fedorov alexandr <fedorov4433@mail.ru>
2021-11-29 00:27:55 +0500

    cp-bound 2

--- CPU-bound.py
+++ CPU-bound.py
@@ -1,14 +1,17 @@
 from hashlib import md5
 from random import choice
 import time
+import concurrent.futures
+
 start = time.time()
-while True:
-    s = "".join([choice("0123456789") for i in range(50)])
-    h = md5(s.encode('utf8')).hexdigest()
+workers_amount = 100
+with concurrent.futures.ProcessPoolExecutor(max_workers=workers_amount) as executor:
+    while True:
+        s = "".join([choice("0123456789") for i in range(50)])
+        h = md5(s.encode('utf8')).hexdigest()
 
-    if h.endswith("00000"):
-        print(s, h)
-        break
-
+        if h.endswith("00000"):
+            print(s, h)
+            break
 end = time.time()
 print(end - start)
